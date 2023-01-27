c14b54a294cff12c93bf9c857721eecdd2b76e69
dityaardi <aditya29122016@gmail.com>
2023-01-27 09:56:45 +0700

    Read perusahaan

--- apkdatamedian/views.py
+++ apkdatamedian/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+def signin(request):
+    return render(request,'sign-in.html')
+
 def dashboard(request):
     return render(request, 'index.html')
 
--- dataperusahaan/views.py
+++ dataperusahaan/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Dataperusahaan
 
 # Create your views here.
+def list_perusahaan(request):
+    list = Dataperusahaan.objects.all()
+    return render(request, 'list_perusahaan.html', {'list': list})
