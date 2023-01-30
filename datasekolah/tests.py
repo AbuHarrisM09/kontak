c5ca52348ed4115e96b182a2c89942546b96ec04
AbuHarrisM09 <abuharismuhyidin@gmail.com>
2023-01-30 15:29:09 +0700

    test datasekolah & datasiswa

--- datasekolah/tests.py
+++ datasekolah/tests.py
@@ -1,11 +1,45 @@
 from django.test import TestCase
-from .models import Datasekolah
+from django.contrib.auth.models import User
+from .models import Datasekolah, StatusSekolah
 
-class SekolahTestCase(TestCase):
+class DatasekolahTestCase(TestCase):
     def setUp(self):
-        Datasekolah.objects.create(npsn="2026822X", nama="SMKN 1 Cimahi", status="Negeri")
-    
-    def test_sekolah_cek_nama(self):
-        """ Cek nama sekolah """
-        smkn1 = Datasekolah.objects.get(nama="SMKN 1 Cimahi")
-        self.assertEqual(smkn1.nama, "SMKN 1 Cimahi")
+        self.user = User.objects.create_user(
+            username='testuser', password='testpassword'
+        )
+        self.sekolah = Datasekolah.objects.create(
+            npsn='1234567890',
+            nama='Sekolah Test',
+            email='sekolah@test.com',
+            hp='081234567890',
+            alamat='Jl. Test 123',
+            provinsi='Jawa Barat',
+            kabupaten_kota='Bandung',
+            kecamatan='Cibiru',
+            status=StatusSekolah.SWASTA,
+            created_by=self.user
+        )
+
+    def test_create_sekolah(self):
+        sekolah = Datasekolah.objects.get(nama='Sekolah Test')
+        self.assertEqual(sekolah.npsn, '1234567890')
+        self.assertEqual(sekolah.email, 'sekolah@test.com')
+        self.assertEqual(sekolah.hp, '081234567890')
+        self.assertEqual(sekolah.alamat, 'Jl. Test 123')
+        self.assertEqual(sekolah.provinsi, 'Jawa Barat')
+        self.assertEqual(sekolah.kabupaten_kota, 'Bandung')
+        self.assertEqual(sekolah.kecamatan, 'Cibiru')
+        self.assertEqual(sekolah.status, StatusSekolah.SWASTA)
+        self.assertEqual(sekolah.created_by, self.user)
+
+    def test_update_sekolah(self):
+        sekolah = Datasekolah.objects.get(nama='Sekolah Test')
+        sekolah.nama = 'Sekolah Test Update'
+        sekolah.save()
+        sekolah = Datasekolah.objects.get(nama='Sekolah Test Update')
+        self.assertEqual(sekolah.nama, 'Sekolah Test Update')
+
+    def test_delete_sekolah(self):
+        sekolah = Datasekolah.objects.get(nama='Sekolah Test')
+        sekolah.delete()
+        self.assertEqual(Datasekolah.objects.count(), 0)
--- datasiswa/tests.py
+++ datasiswa/tests.py
@@ -1,43 +1,39 @@
 from django.test import TestCase
-from .models import Datasiswa
+from datetime import date
+from .models import Datasiswa, JenisKelamin
 
-class StudentTestCase(TestCase):
+class DatasiswaTestCase(TestCase):
     def setUp(self):
-        # create test data
-        self.student = Datasiswa.objects.create(
-            nama='John Doe',
-            email='john@gmail.com',
-            hp='08123456789',
-            alamat='New York',
-            jenis_kelamin='L',
-            tempat_lahir='Bandung'
-            
-            )
+        self.data = {
+            'nama': 'John Doe',
+            'email': 'johndoe@example.com',
+            'hp': '081234567890',
+            'alamat': 'Jl. Example No. 1',
+            'jenis_kelamin': JenisKelamin.LAKILAKI,
+            'tempat_lahir': 'Jakarta',
+            'tanggal_lahir': date(2000, 1, 1),
+            'nisn': '123456789'
+        }
 
-    def test_create_student(self):
-        # test creating a new student
-        new_student = Datasiswa.objects.create(nama='John Doe',
-            email='john@gmail.com',
-            hp='08123456789',
-            alamat='New York',
-            jenis_kelamin='L',
-            tempat_lahir='Bandung')
-        self.assertEqual(Datasiswa.objects.count(), 2)
-        self.assertEqual(new_student.name, 'Jane Smith')
+    def test_create_datasiswa(self):
+        datasiswa = Datasiswa.objects.create(**self.data)
+        self.assertIsInstance(datasiswa, Datasiswa)
+        self.assertEqual(datasiswa.nama, self.data['nama'])
+        self.assertEqual(datasiswa.email, self.data['email'])
+        self.assertEqual(datasiswa.hp, self.data['hp'])
+        self.assertEqual(datasiswa.alamat, self.data['alamat'])
+        self.assertEqual(datasiswa.jenis_kelamin, self.data['jenis_kelamin'])
+        self.assertEqual(datasiswa.tempat_lahir, self.data['tempat_lahir'])
+        self.assertEqual(datasiswa.tanggal_lahir, self.data['tanggal_lahir'])
+        self.assertEqual(datasiswa.nisn, self.data['nisn'])
 
-    def test_read_student(self):
-        # test reading a student
-        student = Datasiswa().objects.get(id=self.student.id)
-        self.assertEqual(student.name, 'John Doe')
+    def test_update_datasiswa(self):
+        datasiswa = Datasiswa.objects.create(**self.data)
+        datasiswa.nama = 'Jane Doe'
+        datasiswa.save()
+        self.assertEqual(datasiswa.nama, 'Jane Doe')
 
-    def test_update_student(self):
-        # test updating a student
-        self.student.name = 'John Smith'
-        self.student.save()
-        student = Datasiswa().objects.get(id=self.student.id)
-        self.assertEqual(student.name, 'John Smith')
-
-    def test_delete_student(self):
-        # test deleting a student
-        self.student.delete()
-        self.assertEqual(Datasiswa().objects.count(), 0)
+    def test_delete_datasiswa(self):
+        datasiswa = Datasiswa.objects.create(**self.data)
+        datasiswa.delete()
+        self.assertEqual(Datasiswa.objects.count(), 0)
