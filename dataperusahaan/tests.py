bca790f231bb2d0cd33a6cd56bb737050010d0ae
AbuHarrisM09 <abuharismuhyidin@gmail.com>
2023-01-30 15:13:23 +0700

    test dataperusahaan

--- dataperusahaan/tests.py
+++ dataperusahaan/tests.py
@@ -1,19 +1,31 @@
 from django.test import TestCase
 from .models import Dataperusahaan, JenisPerusahaan
 
-class DataperusahaanModelTestCase(TestCase):
-    def test_create_dataperusahaan(self):
-        dataperusahaan = Dataperusahaan.objects.create(
-            nama='Perusahaan X',
-            email='perusahaanx@example.com',
-            web='https://perusahaanx.com',
-            hp='+62123456789',
-            alamat='Jalan X No. Y, Kota Z',
-            jenis_perusahaan=JenisPerusahaan.PT,
+class DataperusahaanTestCase(TestCase):
+    def setUp(self):
+        Dataperusahaan.objects.create(
+            nama="Perusahaan A",
+            email="perusahaan_a@example.com",
+            jenis_perusahaan=JenisPerusahaan.PT
         )
-        self.assertEqual(dataperusahaan.nama, 'Perusahaan X')
-        self.assertEqual(dataperusahaan.email, 'perusahaanx@example.com')
-        self.assertEqual(dataperusahaan.web, 'https://perusahaanx.com')
-        self.assertEqual(dataperusahaan.hp, '+62123456789')
-        self.assertEqual(dataperusahaan.alamat, 'Jalan X No. Y, Kota Z')
-        self.assertEqual(dataperusahaan.jenis_perusahaan, JenisPerusahaan.PT)
+
+    def test_create(self):
+        perusahaan = Dataperusahaan.objects.get(nama="Perusahaan A")
+        self.assertEqual(perusahaan.nama, "Perusahaan A")
+        self.assertEqual(perusahaan.email, "perusahaan_a@example.com")
+        self.assertEqual(perusahaan.jenis_perusahaan, "PT")
+
+    def test_update(self):
+        perusahaan = Dataperusahaan.objects.get(nama="Perusahaan A")
+        perusahaan.nama = "Perusahaan B"
+        perusahaan.save()
+
+        updated_perusahaan = Dataperusahaan.objects.get(id=perusahaan.id)
+        self.assertEqual(updated_perusahaan.nama, "Perusahaan B")
+
+    def test_delete(self):
+        perusahaan = Dataperusahaan.objects.get(nama="Perusahaan A")
+        perusahaan.delete()
+
+        with self.assertRaises(Dataperusahaan.DoesNotExist):
+            Dataperusahaan.objects.get(id=perusahaan.id)
